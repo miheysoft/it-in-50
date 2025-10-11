b9c6cd71a018c4abe4bdeb96c9c5b374cdbe5ac6
it_50 <skbest@yandex.ru>
2025-10-11 15:55:10 +0700

    запускаю gitpages

--- init_mkdocs.sh
+++ init_mkdocs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-. /home/miheev/it-in-50/.venv/bin/activate
-screen -d -m -S mkdocs mkdocs serve --dev-addr='192.168.1.6:8000'
+. /home/miheev/it-in-50/.venv/bin/activate 
+screen -d -m -S mkdocs mkdocs serve --dev-addr='192.168.1.6:8000' 
